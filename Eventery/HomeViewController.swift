13ff0918c7f24f44ad286f94af5c24a7256802a5
Andrew Chiu <andrewchiu198@gmail.com>
2023-04-28 17:58:58 -0400

    TabBar Architecture

---
+++ Eventery/HomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Eventery
+//
+//  Created by Andrew Chiu on 4/28/23.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        UITabBar.appearance().barTintColor = .white
+        tabBar.backgroundColor = UIColor(named: "TabBarColor")
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.borderColor = UIColor.separator.cgColor
+        tabBar.unselectedItemTintColor = .white
+        tabBar.tintColor = UIColor(named: "HighlightColor")
+        setupVCs()
+    }
+        
+        func setupVCs() {
+            viewControllers = [
+                createNavController(for: EventsViewController(), title: NSLocalizedString("Events", comment: ""), image: UIImage(systemName: "magnifyingglass")!),
+                
+                createNavController(for: CalendarViewController(), title: NSLocalizedString("Calendar", comment: ""), image: UIImage(systemName: "calendar")!),
+                
+                createNavController(for: MapViewController(), title: NSLocalizedString("Map", comment: ""), image: UIImage(systemName: "map.fill")!),
+                
+                createNavController(for: PostViewController(), title: NSLocalizedString("Post", comment: ""), image: UIImage(systemName: "square.and.pencil")!),
+                
+                createNavController(for: ProfileViewController(), title: NSLocalizedString("Profile", comment: ""), image: UIImage(systemName: "person.fill")!),
+        ]
+    }
+    
+    fileprivate func createNavController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+    
+}
+
